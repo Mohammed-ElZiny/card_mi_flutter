d6ea353e1c44cf601032aea35de98bad2113053e
Mohammed-ElZiny <mohammed4ziny@gmail.com>
2021-11-21 13:40:00 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,48 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        home: MainPage(),
+      ),
+    );
+
+
+class MainPage extends StatefulWidget {
+
+  @override
+  _MainPageState createState() => _MainPageState();
 }
 
-class MyApp extends StatelessWidget {
+class _MainPageState extends State<MainPage> {
+  int ballNum  = 1;
+  void changeBall(){
+    setState(  ( ){ ballNum = Random().nextInt(5)+1; }   );
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 100,
-              backgroundImage: AssetImage("images/acv.jpg"),
-            ),
-            Text(
-              "Muhammed Ali",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.redAccent,
-                fontFamily: "pacifico",
-                letterSpacing: 2,
-              ),
-            ),
-            Text("Flutter Developer",
-            style:TextStyle(
-              fontFamily: "regular",
-              color:  Colors.redAccent,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 5,
-            )
-            ),
-            SizedBox(
-              width: double.infinity,
-            ),
-            Container(
-              color: Colors.redAccent,
-              padding:EdgeInsets.symmetric(vertical: 10,horizontal: 25),
-              margin:EdgeInsets.symmetric(vertical: 16,horizontal: 16),
-              child: Row(
-                children:[
-                  Icon(Icons.phone,
-                  size:50,
-                    color: Colors.tealAccent,
-                  ),
-                  SizedBox(width: 10),
-                  Text("+2011579436",
-                      style:TextStyle(
-                        fontFamily: "regular",
-                        color:  Colors.tealAccent,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 5,
-                      ),
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask me anything"),
+      ),
+      body:SafeArea(
+        child:Container(
+          color: Colors.blue,
+          child: Center(
+            child:Padding(
+              padding: EdgeInsets.all(16),
+              child: TextButton(
+                child: Image.asset('images/ball$ballNum.png'),
+                onPressed: ()=>changeBall(),
+              )
 
-                ],
-              ),
             ),
-            Container(
-              color: Colors.redAccent,
-              padding:EdgeInsets.symmetric(vertical: 10,horizontal: 25),
-              margin:EdgeInsets.symmetric(vertical: 16,horizontal: 16),
-              child: Row(
-                children:[
-                  Icon(Icons.mail,
-                  size:50,
-                    color: Colors.tealAccent,
-                  ),
-                  SizedBox(width: 10),
-                  Text("Mohammed4ziny@gmail.com",
-                      style:TextStyle(
-                        fontFamily: "regular",
-                        color:  Colors.tealAccent,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 4,
-                      ),
-                  ),
-
-                ],
-              ),
-            ),
-
-
-
-          ],
-        )),
+          ),
+        ),
       ),
     );
   }
 }
+
+
